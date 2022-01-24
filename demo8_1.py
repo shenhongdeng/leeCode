140e2e4edefc5caa8e13a56bd8a4d9fcac429869
shenhongdeng <xk20shd@163.com>
2022-01-24 23:39:55 +0800

    add demo8

---
+++ demo8_1.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class         
+
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int):
+
+
